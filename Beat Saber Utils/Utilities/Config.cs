8a42f884b1431109c19e30dbf052ee7426c40a7d
Kyle1413 <kylet1412@gmail.com>
2019-01-18 00:13:14 -0500

    Support for Mods to make their own Config Files

---
+++ Beat Saber Utils/Utilities/Config.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+
+namespace BS_Utils.Utilities
+{
+    public class Config
+    {
+        private IniFile _instance;
+
+        public Config(string name)
+        {
+            _instance = new IniFile(Path.Combine(Environment.CurrentDirectory, $"UserData/{name}.ini"));
+        }
+
+
+        /// <summary>
+        /// Gets a string from the ini.
+        /// </summary>
+        /// <param name="section">Section of the key.</param>
+        /// <param name="name">Name of the key.</param>
+        /// <param name="defaultValue">Value that should be used when no value is found.</param>
+        /// <param name="autoSave">Whether or not the default value should be written if no value is found.</param>
+        /// <returns></returns>
+        public string GetString(string section, string name, string defaultValue = "", bool autoSave = false)
+        {
+            string value = _instance.IniReadValue(section, name);
+            if (value != "")
+                return value;
+            else if (autoSave)
+                SetString(section, name, defaultValue);
+
+            return defaultValue;
+        }
+
+        /// <summary>
+        /// Gets an int from the ini.
+        /// </summary>
+        /// <param name="section">Section of the key.</param>
+        /// <param name="name">Name of the key.</param>
+        /// <param name="defaultValue">Value that should be used when no value is found.</param>
+        /// <param name="autoSave">Whether or not the default value should be written if no value is found.</param>
+        /// <returns></returns>
+        public int GetInt(string section, string name, int defaultValue = 0, bool autoSave = false)
+        {
+            int value;
+            if (int.TryParse(_instance.IniReadValue(section, name), out value))
+                return value;
+            else if (autoSave)
+                SetInt(section, name, defaultValue);
+
+            return defaultValue;
+        }
+
+
+        /// <summary>
+        /// Gets a float from the ini.
+        /// </summary>
+        /// <param name="section">Section of the key.</param>
+        /// <param name="name">Name of the key.</param>
+        /// <param name="defaultValue">Value that should be used when no value is found.</param>
+        /// <param name="autoSave">Whether or not the default value should be written if no value is found.</param>
+        /// <returns></returns>
+        public float GetFloat(string section, string name, float defaultValue = 0f, bool autoSave = false)
+        {
+            float value;
+            if (float.TryParse(_instance.IniReadValue(section, name), out value))
+                return value;
+            else if (autoSave)
+                SetFloat(section, name, defaultValue);
+
+            return defaultValue;
+        }
+
+        /// <summary>
+        /// Gets a bool from the ini.
+        /// </summary>
+        /// <param name="section">Section of the key.</param>
+        /// <param name="name">Name of the key.</param>
+        /// <param name="defaultValue">Value that should be used when no value is found.</param>
+        /// <param name="autoSave">Whether or not the default value should be written if no value is found.</param>
+        /// <returns></returns>
+        public bool GetBool(string section, string name, bool defaultValue = false, bool autoSave = false)
+        {
+            string sVal = GetString(section, name, null);
+            if (sVal == "1" || sVal == "0")
+            {
+                return sVal == "1";
+            }
+            else if (autoSave)
+            {
+                SetBool(section, name, defaultValue);
+            }
+
+            return defaultValue;
+        }
+
+
+        /// <summary>
+        /// Checks whether or not a key exists in the ini.
+        /// </summary>
+        /// <param name="section">Section of the key.</param>
+        /// <param name="name">Name of the key.</param>
+        /// <returns></returns>
+        public bool HasKey(string section, string name)
+        {
+            return _instance.IniReadValue(section, name) != null;
+        }
+
+        /// <summary>
+        /// Sets a float in the ini.
+        /// </summary>
+        /// <param name="section">Section of the key.</param>
+        /// <param name="name">Name of the key.</param>
+        /// <param name="value">Value that should be written.</param>
+        public void SetFloat(string section, string name, float value)
+        {
+            _instance.IniWriteValue(section, name, value.ToString());
+        }
+
+        /// <summary>
+        /// Sets an int in the ini.
+        /// </summary>
+        /// <param name="section">Section of the key.</param>
+        /// <param name="name">Name of the key.</param>
+        /// <param name="value">Value that should be written.</param>
+        public void SetInt(string section, string name, int value)
+        {
+            _instance.IniWriteValue(section, name, value.ToString());
+
+        }
+
+        /// <summary>
+        /// Sets a string in the ini.
+        /// </summary>
+        /// <param name="section">Section of the key.</param>
+        /// <param name="name">Name of the key.</param>
+        /// <param name="value">Value that should be written.</param>
+        public void SetString(string section, string name, string value)
+        {
+            _instance.IniWriteValue(section, name, value);
+
+        }
+
+        /// <summary>
+        /// Sets a bool in the ini.
+        /// </summary>
+        /// <param name="section">Section of the key.</param>
+        /// <param name="name">Name of the key.</param>
+        /// <param name="value">Value that should be written.</param>
+        public void SetBool(string section, string name, bool value)
+        {
+            _instance.IniWriteValue(section, name, value ? "1" : "0");
+
+        }
+
+
+
+
+    }
+}
