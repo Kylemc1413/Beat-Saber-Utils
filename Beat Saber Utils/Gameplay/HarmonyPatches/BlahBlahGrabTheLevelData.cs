950a86cbf88bf630516c0eced545c6a69ad54acb
dcljr <dcljr@me.com>
2019-06-13 01:33:26 -0700

    added GameplayCoreSceneSetupData for campaign

--- Beat Saber Utils/Gameplay/HarmonyPatches/BlahBlahGrabTheLevelData.cs
+++ Beat Saber Utils/Gameplay/HarmonyPatches/BlahBlahGrabTheLevelData.cs
@@ -21,4 +21,23 @@ namespace BS_Utils.Gameplay.HarmonyPatches
             Plugin.TriggerLevelFinishEvent(levelScenesTransitionSetupDataSO, levelCompletionResults);
         }
     }
+    
+    [HarmonyPatch(typeof(MissionLevelScenesTransitionSetupDataSO))]
+    [HarmonyPatch("Init", MethodType.Normal)]
+    class BlahBlahGrabTheMissionLevelData
+    {
+        static void Prefix(MissionLevelScenesTransitionSetupDataSO __instance, IDifficultyBeatmap difficultyBeatmap, MissionObjective[] missionObjectives, GameplayModifiers gameplayModifiers, PlayerSpecificSettings playerSpecificSettings)
+        {
+            Plugin.LevelData.GameplayCoreSceneSetupData = new GameplayCoreSceneSetupData(difficultyBeatmap, gameplayModifiers, playerSpecificSettings, PracticeSettings.defaultPracticeSettings, false);
+            Plugin.LevelData.IsSet = true;
+            __instance.didFinishEvent -= __instance_didFinishEvent;
+            __instance.didFinishEvent += __instance_didFinishEvent;
+
+        }
+
+        private static void __instance_didFinishEvent(MissionLevelScenesTransitionSetupDataSO missionLevelScenesTransitionSetupDataSO, MissionCompletionResults missionCompletionResults)
+        {
+            Plugin.TriggerMissionFinishEvent(missionLevelScenesTransitionSetupDataSO, missionCompletionResults);
+        }
+    }
 }
