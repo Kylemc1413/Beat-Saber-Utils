791fabfb7bd0e031734d64a33acd3430c5fb32e3
Kyle1413 <kylet1412@gmail.com>
2019-12-16 16:07:32 -0500

    Add UIUtilities

--- Beat Saber Utils/Gameplay/HarmonyPatches/ResultsViewControllerSetDataToUI.cs
+++ Beat Saber Utils/Gameplay/HarmonyPatches/ResultsViewControllerSetDataToUI.cs
@@ -8,16 +8,26 @@ namespace BS_Utils.Gameplay.HarmonyPatches
     [HarmonyPatch("SetDataToUI", MethodType.Normal)]
     class ResultsViewControllerSetDataToUI
     {
-        static void Postfix(ref TextMeshProUGUI ____clearedDifficultyText)
+        static void Postfix(ref TextMeshProUGUI ____clearedDifficultyText, ref TextMeshProUGUI ____failedDifficultyText)
         {
             ____clearedDifficultyText.overflowMode = TextOverflowModes.Overflow;
             ____clearedDifficultyText.richText = true;
+            ____failedDifficultyText.overflowMode = TextOverflowModes.Overflow;
+            ____failedDifficultyText.richText = true;
 
             if (ScoreSubmission.disabled || ScoreSubmission.prolongedDisable)
-                ____clearedDifficultyText.text += "  \r\n<color=#ff0000ff><size=55%><b>Score Submission Disabled by: " +
+            {
+                ____clearedDifficultyText.text += "  \r\n<color=#ff0000ff><size=60%><b>Score Submission Disabled by: " +
                     ScoreSubmission.ModString +
                     " | " +
                     ScoreSubmission.ProlongedModString;
+
+               ____failedDifficultyText.text += "  \r\n<color=#ff0000ff><size=60%><b>Score Submission Disabled by: " +
+                    ScoreSubmission.ModString +
+                    " | " +
+                    ScoreSubmission.ProlongedModString;
+            }
+
 
             ScoreSubmission.ModList.Clear();
             ScoreSubmission.disabled = false;
---
+++ Beat Saber Utils/Utilities/UiUtilities.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using System.Reflection;
+using System.IO;
+namespace BS_Utils.Utilities
+{
+    public class UIUtilities
+    {
+
+        public static Texture2D LoadTextureRaw(byte[] file)
+        {
+            if (file.Count() > 0)
+            {
+                Texture2D Tex2D = new Texture2D(2, 2);
+                if (Tex2D.LoadImage(file))
+                    return Tex2D;
+            }
+            return null;
+        }
+
+        public static Texture2D LoadTextureFromFile(string FilePath)
+        {
+            if (File.Exists(FilePath))
+                return LoadTextureRaw(File.ReadAllBytes(FilePath));
+
+            return null;
+        }
+
+        public static Texture2D LoadTextureFromResources(string resourcePath)
+        {
+            return LoadTextureRaw(GetResource(Assembly.GetCallingAssembly(), resourcePath));
+        }
+
+        public static Sprite LoadSpriteRaw(byte[] image, float PixelsPerUnit = 100.0f)
+        {
+            return LoadSpriteFromTexture(LoadTextureRaw(image), PixelsPerUnit);
+        }
+
+        public static Sprite LoadSpriteFromTexture(Texture2D SpriteTexture, float PixelsPerUnit = 100.0f)
+        {
+            if (SpriteTexture)
+                return Sprite.Create(SpriteTexture, new Rect(0, 0, SpriteTexture.width, SpriteTexture.height), new Vector2(0, 0), PixelsPerUnit);
+            return null;
+        }
+
+        public static Sprite LoadSpriteFromFile(string FilePath, float PixelsPerUnit = 100.0f)
+        {
+            return LoadSpriteFromTexture(LoadTextureFromFile(FilePath), PixelsPerUnit);
+        }
+
+        public static Sprite LoadSpriteFromResources(string resourcePath, float PixelsPerUnit = 100.0f)
+        {
+            return LoadSpriteRaw(GetResource(Assembly.GetCallingAssembly(), resourcePath), PixelsPerUnit);
+        }
+
+        public static byte[] GetResource(Assembly asm, string ResourceName)
+        {
+            System.IO.Stream stream = asm.GetManifestResourceStream(ResourceName);
+            byte[] data = new byte[stream.Length];
+            stream.Read(data, 0, (int)stream.Length);
+            return data;
+        }
+
+        public static void PrintHierarchy(Transform transform, string spacing = "|-> ")
+        {
+            spacing = spacing.Insert(1, "  ");
+            var tempList = transform.Cast<Transform>().ToList();
+            foreach (var child in tempList)
+            {
+                Console.WriteLine($"{spacing}{child.name}");
+                PrintHierarchy(child, "|" + spacing);
+            }
+        }
+    }
+}
