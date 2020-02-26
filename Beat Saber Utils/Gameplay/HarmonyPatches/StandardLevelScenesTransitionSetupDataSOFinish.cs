b7b46f3854a67dca806c1798f9735e6efe417e4e
Auros <aurosians@gmail.com>
2020-02-25 22:16:48 -0500

    Merged score submission stuff

---
+++ Beat Saber Utils/Gameplay/HarmonyPatches/StandardLevelScenesTransitionSetupDataSOFinish.cs
@@ -0,0 +1,19 @@
+﻿using Harmony;
+using System;
+
+namespace BS_Utils.Gameplay.HarmonyPatches
+{
+    [HarmonyPatch(typeof(StandardLevelScenesTransitionSetupDataSO))]
+    [HarmonyPatch("Finish", MethodType.Normal)]
+    class StandardLevelScenesTransitionSetupDataSOFinishPatch
+    {
+        static void Prefix(LevelCompletionResults levelCompletionResults)
+        {
+            if (ScoreSubmission.disabled || ScoreSubmission.prolongedDisable)
+            {
+                //Utilities.Logger.Log("Score Submission Disabled");
+                ScoreSubmission.DisableScoreSaberScoreSubmission();
+            }
+        }
+    }
+}
