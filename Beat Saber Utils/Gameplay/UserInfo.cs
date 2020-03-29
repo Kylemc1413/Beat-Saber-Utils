dad28c0761365beeb1ea2a1238277ae975fd6dd8
Zingabopp <Zingabopp@gmail.com>
2020-03-28 20:44:23 -0500

    More robust UpdateUserInfo

--- Beat Saber Utils/Gameplay/UserInfo.cs
+++ Beat Saber Utils/Gameplay/UserInfo.cs
@@ -8,10 +8,13 @@ using LogLevel = IPA.Logging.Logger.Level;
 using System.Linq;
 using System.Reflection;
 using System.IO;
+using System.Collections;
+
 namespace BS_Utils.Gameplay
 {
     public static class GetUserInfo
     {
+        static PlatformInfo platformInfo;
         static string userName = null;
         static ulong userID = 0;
         static Texture2D userAvatar = null;
@@ -30,6 +33,19 @@ namespace BS_Utils.Gameplay
             }
         }
         private static VRPlatformHelper _vRPlatformHelper;
+        private static PlatformUserModelSO _platformUserModel;
+
+        public static PlatformUserModelSO PlatformUserModelSO
+        {
+            get
+            {
+                if (_platformUserModel == null)
+                    _platformUserModel = ScriptableObject.CreateInstance<PlatformUserModelSO>();
+                return _platformUserModel;
+            }
+            internal set { _platformUserModel = value; }
+        }
+
         static GetUserInfo()
         {
             UpdateUserInfo();
@@ -37,25 +53,13 @@ namespace BS_Utils.Gameplay
         
         public static void UpdateUserInfo()
         {
+            if (getUserActive)
+                return; // Already retrieving user info.
             if (userID == 0 || userName == null)
             {
                 try
                 {
-                    if (vRPlatformHelper.vrPlatformSDK == VRPlatformHelper.VRPlatformSDK.OpenVR || Environment.CommandLine.Contains("-vrmode oculus"))
-                    {
-                        Logger.Log("Attempting to Grab Steam User", LogLevel.Debug);
-                        GetSteamUser();
-                    }
-                    else if (vRPlatformHelper.vrPlatformSDK == VRPlatformHelper.VRPlatformSDK.Oculus)
-                    {
-                        Logger.Log("Attempting to Grab Oculus User", LogLevel.Debug);
-                        GetOculusUser();
-                    }
-                    else if (Environment.CommandLine.Contains("fpfc") && vRPlatformHelper.vrPlatformSDK == VRPlatformHelper.VRPlatformSDK.Unknown)
-                    {
-                        Logger.Log("Attempting to Grab Steam User", LogLevel.Debug);
-                        GetSteamUser();
-                    }
+                    SharedCoroutineStarter.instance.StartCoroutine(GetUserCoroutine());
                 }
                 catch (Exception e)
                 {
@@ -64,35 +68,105 @@ namespace BS_Utils.Gameplay
                 }
             }
         }
-
-        internal static void GetSteamUser()
+        private static bool getUserActive = false;
+        private static bool foundUser = false;
+        private static bool waitingForCompletion = false;
+        private static IEnumerator GetUserCoroutine()
         {
-            if (SteamManager.Initialized)
+            getUserActive = true;
+            WaitForSeconds wait = new WaitForSeconds(5f);
+            int tries = 1;
+            try
             {
-                var steamUser = SteamUser.GetSteamID();
+                while (!foundUser && tries < 10)
+                {
+                    if (!waitingForCompletion)
+                    {
+                        if (!foundUser)
+                        {
+                            Logger.log.Debug($"Detected platform: {PlatformUserModelSO.platformInfo.platform}");
+                            try
+                            {
+                                waitingForCompletion = true;
+                                PlatformUserModelSO.GetUserInfo(UserInfoCompletionHandler);
+                            }
+                            catch (Exception ex)
+                            {
+                                waitingForCompletion = false;
+                                Logger.log.Error($"Error retrieving user info: {ex.Message}");
+                                Logger.log.Debug(ex);
+                            }
+                            tries++;
+                        }
+                    }
+                    yield return wait;
+                }
+            }
+            finally
+            {
+                getUserActive = false;
+            }
+        }
 
-                userName = SteamFriends.GetPersonaName();
-                userID = steamUser.m_SteamID;
-                userAvatar = GetAvatar(steamUser);
+        private static void UserInfoCompletionHandler(PlatformUserModelSO.GetUserInfoResult result, PlatformUserModelSO.UserInfo userInfo)
+        {
+            if (result == PlatformUserModelSO.GetUserInfoResult.OK)
+            {
+                Logger.log.Debug($"UserInfo found: {userInfo.userId}: {userInfo.userName}");
+                platformInfo = PlatformUserModelSO.platformInfo;
+                if (ulong.TryParse(userInfo.userId, out ulong id))
+                    userID = id;
+                else
+                {
+                    Logger.log.Warn($"Unable to parse {userInfo.userId} as a ulong.");
+                    userID = 0;
+                }
+                userName = userInfo.userName;
+                if (PlatformUserModelSO.platformInfo.platform == PlatformInfo.Platform.Steam)
+                {
+                    if (SteamManager.Initialized)
+                    {
+                        var steamUser = SteamUser.GetSteamID();
+                        userAvatar = GetAvatar(steamUser);
+                    }
+                }
+                else if(PlatformUserModelSO.platformInfo.platform == PlatformInfo.Platform.Oculus)
+                    userAvatar = LoadTextureFromResources("BS_Utils.Resources.oculus.png");
+                foundUser = true;
             }
             else
-            {
-                Logger.Log("Steam is not initialized!", LogLevel.Warning);
-            }
+                Logger.log.Error("Failed to retrieve user info.");
+            waitingForCompletion = false;
         }
 
-        internal static void GetOculusUser()
-        {
-            Users.GetLoggedInUser().OnComplete((Message<User> msg) =>
-            {
-                if (!msg.IsError)
-                {
-                    userID = msg.Data.ID;
-                    userName = msg.Data.OculusID;
-                    userAvatar = LoadTextureFromResources("BS_Utils.Resources.oculus.png");
-                }
-            });
-        }
+        //internal static void GetSteamUser()
+        //{
+        //    if (SteamManager.Initialized)
+        //    {
+        //        var steamUser = SteamUser.GetSteamID();
+
+        //        userName = SteamFriends.GetPersonaName();
+        //        userID = steamUser.m_SteamID;
+        //        userAvatar = GetAvatar(steamUser);
+        //    }
+        //    else
+        //    {
+        //        Logger.Log("Steam is not initialized!", LogLevel.Warning);
+        //    }
+        //}
+
+        //internal static void GetOculusUser()
+        //{
+        //    Users.GetLoggedInUser().OnComplete((Message<User> msg) =>
+        //    {
+        //        if (!msg.IsError)
+        //        {
+        //            userID = msg.Data.ID;
+        //            userName = msg.Data.OculusID;
+        //            userAvatar = LoadTextureFromResources("BS_Utils.Resources.oculus.png");
+        //        }
+        //    });
+        //}
 
         private static Texture2D GetAvatar(CSteamID steamUser)
         {
@@ -118,6 +192,10 @@ namespace BS_Utils.Gameplay
                 Debug.LogError("Couldn't get avatar.");
                 return new Texture2D(0, 0);
             }
+        }
+        public static PlatformInfo GetPlatformInfo()
+        {
+            return platformInfo;
         }
 
         public static string GetUserName()
