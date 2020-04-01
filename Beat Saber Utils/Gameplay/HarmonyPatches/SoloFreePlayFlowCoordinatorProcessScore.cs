84e2f5a0113ad15ca872b0a40bb679bc30614e4c
Kyle1413 <kylet1412@gmail.com>
2020-04-01 13:24:09 -0400

    Fix another oopsie

--- Beat Saber Utils/Gameplay/HarmonyPatches/SoloFreePlayFlowCoordinatorProcessScore.cs
+++ Beat Saber Utils/Gameplay/HarmonyPatches/SoloFreePlayFlowCoordinatorProcessScore.cs
@@ -4,16 +4,17 @@ using HarmonyLib;
 namespace BS_Utils.Gameplay.HarmonyPatches
 {
     [HarmonyPatch(typeof(SoloFreePlayFlowCoordinator))]
-    [HarmonyPatch("ProcessLevelCompletionResultsAfterLevelDidFinish", MethodType.Normal)]
+    [HarmonyPatch("ProcessScore", MethodType.Normal)]
     class SoloFreePlayFlowCoordinatorProcessScore
     {
-        static void Prefix(LevelCompletionResults levelCompletionResults, ref bool practice)
+        static bool Prefix()
         {
             if (ScoreSubmission.WasDisabled || ScoreSubmission.disabled || ScoreSubmission.prolongedDisable)
             {
                 //Utilities.Logger.Log($"Score Submission Disabled by {string.Join("|", ScoreSubmission.LastDisablers)}");
-                practice = true;
+                return false;
             }
+            return true;
         }
 
         static void Postfix(LevelCompletionResults levelCompletionResults, ref bool practice) { }
