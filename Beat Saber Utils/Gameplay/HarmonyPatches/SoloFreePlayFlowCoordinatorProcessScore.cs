77df600ee286d680c0eed8370fc8e5b86a5b5230
Kyle1413 <kylet1412@gmail.com>
2020-04-01 13:49:57 -0400

    Revert "Fix another oopsie"
    This reverts commit 84e2f5a0113ad15ca872b0a40bb679bc30614e4c.

--- Beat Saber Utils/Gameplay/HarmonyPatches/SoloFreePlayFlowCoordinatorProcessScore.cs
+++ Beat Saber Utils/Gameplay/HarmonyPatches/SoloFreePlayFlowCoordinatorProcessScore.cs
@@ -4,17 +4,16 @@ using HarmonyLib;
 namespace BS_Utils.Gameplay.HarmonyPatches
 {
     [HarmonyPatch(typeof(SoloFreePlayFlowCoordinator))]
-    [HarmonyPatch("ProcessScore", MethodType.Normal)]
+    [HarmonyPatch("ProcessLevelCompletionResultsAfterLevelDidFinish", MethodType.Normal)]
     class SoloFreePlayFlowCoordinatorProcessScore
     {
-        static bool Prefix()
+        static void Prefix(LevelCompletionResults levelCompletionResults, ref bool practice)
         {
             if (ScoreSubmission.WasDisabled || ScoreSubmission.disabled || ScoreSubmission.prolongedDisable)
             {
                 //Utilities.Logger.Log($"Score Submission Disabled by {string.Join("|", ScoreSubmission.LastDisablers)}");
-                return false;
+                practice = true;
             }
-            return true;
         }
 
         static void Postfix(LevelCompletionResults levelCompletionResults, ref bool practice) { }
