d8fde4c5dc32d43e9fc2705eda7c163447a00a15
Kyle1413 <kylet1412@gmail.com>
2020-10-15 19:18:07 -0400

    Update BeatmapCharacteristicSelectionViewControllerDidDeActivate.cs

--- Beat Saber Utils/Gameplay/HarmonyPatches/BeatmapCharacteristicSelectionViewControllerDidDeActivate.cs
+++ Beat Saber Utils/Gameplay/HarmonyPatches/BeatmapCharacteristicSelectionViewControllerDidDeActivate.cs
@@ -3,13 +3,5 @@ using HarmonyLib;
 
 namespace BS_Utils.Gameplay.Harmony_Patches
 {
-    [HarmonyPatch(typeof(BeatmapCharacteristicSegmentedControlController))]
-    [HarmonyPatch("SetData", MethodType.Normal)]
-    class BeatmapCharacteristicSelectionViewControllerDidDeactivate
-    {
-        static void Postfix(BeatmapCharacteristicSegmentedControlController __instance, IDifficultyBeatmapSet[] difficultyBeatmapSets, BeatmapCharacteristicSO selectedBeatmapCharacteristic)
-        {
-            Gamemode.CharacteristicSelectionViewController_didSelectBeatmapCharacteristicEvent(__instance, __instance.selectedBeatmapCharacteristic);
-        }
-    }
+
 }
