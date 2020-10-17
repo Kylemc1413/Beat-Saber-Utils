04c2b235d81936605f6b87480603fd02e292aaea
Kyle1413 <kylet1412@gmail.com>
2020-10-17 10:45:06 -0400

    Make Score Submission disable text actually readable

--- Beat Saber Utils/Gameplay/HarmonyPatches/ResultsViewControllerSetDataToUI.cs
+++ Beat Saber Utils/Gameplay/HarmonyPatches/ResultsViewControllerSetDataToUI.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using HarmonyLib;
+using HMUI;
 using TMPro;
+using UnityEngine;
 
 namespace BS_Utils.Gameplay.HarmonyPatches
 {
@@ -8,19 +10,30 @@ namespace BS_Utils.Gameplay.HarmonyPatches
     [HarmonyPatch("SetDataToUI", MethodType.Normal)]
     class ResultsViewControllerSetDataToUI
     {
-        static void Postfix(ref TextMeshProUGUI ____rankText)
+        static void Postfix(ref TextMeshProUGUI ____rankText, ref GameObject ____clearedBannerGo, ref GameObject ____failedBannerGo)
         {
             ____rankText.overflowMode = TextOverflowModes.Overflow;
             ____rankText.richText = true;
-         //   ____failedDifficultyText.overflowMode = TextOverflowModes.Overflow;
-         //   ____failedDifficultyText.richText = true;
+            //   ____failedDifficultyText.overflowMode = TextOverflowModes.Overflow;
+            //   ____failedDifficultyText.richText = true;
+            GameObject bannerGo = ____clearedBannerGo.activeInHierarchy ? ____clearedBannerGo : ____failedBannerGo;
 
+            GameObject bgGo = bannerGo.transform.Find("BG").gameObject;
+            CurvedTextMeshPro tmp = bannerGo.GetComponentInChildren<CurvedTextMeshPro>();
+            if (bannerGo == ____failedBannerGo)
+                tmp.color = Color.red;
+                if (bgGo != null) bgGo.SetActive(true);
             if (ScoreSubmission.WasDisabled || ScoreSubmission.disabled || ScoreSubmission.prolongedDisable)
             {
-                ____rankText.text += "  \r\n<color=#ff0000ff><size=60%><b>Score Submission Disabled by: " +
+                string color = "<color=#ff0000ff>";
+                string scoresubmissiontext = $"  \r\n{color}<size=60%><b>Score Submission Disabled by: " +
                     ScoreSubmission.LastDisabledModString +
                     " | " +
                     ScoreSubmission.ProlongedModString;
+                tmp.color = Color.white;
+                tmp.text += scoresubmissiontext;
+                if(bgGo != null)
+                    bgGo.SetActive(false);
                 /*
                ____failedDifficultyText.text += "  \r\n<color=#ff0000ff><size=60%><b>Score Submission Disabled by: " +
                     ScoreSubmission.LastDisabledModString +
