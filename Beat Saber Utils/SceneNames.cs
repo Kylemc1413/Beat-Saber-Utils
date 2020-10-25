32261d0703406f4c9c3d36d36c3c9d80fc44d62f
Zingabopp <Zingabopp@gmail.com>
2020-10-25 12:47:32 -0500

    Added SceneNames class

---
+++ Beat Saber Utils/SceneNames.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BS_Utils
+{
+    public class SceneNames
+    {
+        public static readonly string Menu = "MenuViewControllers";
+        public static readonly string PostSongMenu = "MenuCore";
+        public static readonly string Game = "GameCore";
+        public static readonly string HealthWarning = "HealthWarning";
+        public static readonly string EmptyTransition = "EmptyTransition";
+
+    }
+}
