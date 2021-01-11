f285c6b592ff6d4dbf17fe38b40799571927dffb
Zingabopp <Zingabopp@gmail.com>
2021-01-11 12:17:33 -0600

    Moved `LevelFinishedEventArgs` to `Utilities` namespace.

--- Beat Saber Utils/Utilities/LevelFinishedEventArgs.cs
+++ Beat Saber Utils/Utilities/LevelFinishedEventArgs.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BS_Utils
+namespace BS_Utils.Utilities
 {
     public class LevelFinishedEventArgs : EventArgs
     {
