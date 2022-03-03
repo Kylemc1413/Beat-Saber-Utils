8445dba188e6b3ed36d39fa29d8cbd064a18b97f
Eris <erisapps@outlook.be>
2022-03-03 17:36:58 +0100

    Marked Logger class internal
    Consumers should ask their own BSIPA Logger

--- Beat Saber Utils/Utilities/Logger.cs
+++ Beat Saber Utils/Utilities/Logger.cs
@@ -1,18 +1,10 @@
-using System;
 using IPALogger = IPA.Logging.Logger;
 
 namespace BS_Utils.Utilities
 {
-	// TODO: Mark class as internal upon removal of the obsolete method.
-	public class Logger
+	internal class Logger
 	{
 		internal static IPALogger log { get; set; }
-
-		[Obsolete("The logger class will become internal in a future update. Get your own logger from BSIPA instead.")]
-		public static void Log(string modName, string message)
-		{
-			log.Info($"[{modName}]  {message}");
-		}
 
 		internal static void Log(string message)
 		{
