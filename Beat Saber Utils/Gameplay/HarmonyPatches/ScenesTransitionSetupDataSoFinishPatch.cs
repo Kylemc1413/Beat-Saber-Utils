3d9a8133599fed6d343cdebde94c6ebf5bdf12ae
Eris <erisapps@outlook.be>
2023-03-02 00:25:27 +0100

    Remove obsoleted TargetMethods method

--- Beat Saber Utils/Gameplay/HarmonyPatches/ScenesTransitionSetupDataSoFinishPatch.cs
+++ Beat Saber Utils/Gameplay/HarmonyPatches/ScenesTransitionSetupDataSoFinishPatch.cs
@@ -7,12 +7,6 @@ namespace BS_Utils.Gameplay.HarmonyPatches
     [HarmonyPatch]
     internal class ScenesTransitionSetupDataSoFinishPatch
     {
-        private static IEnumerable<MethodBase> TargetMethods()
-        {
-            yield return AccessTools.Method(typeof(StandardLevelScenesTransitionSetupDataSO), nameof(StandardLevelScenesTransitionSetupDataSO.Finish), new[] { typeof(LevelCompletionResults) });
-            yield return AccessTools.Method(typeof(MultiplayerLevelScenesTransitionSetupDataSO), nameof(MultiplayerLevelScenesTransitionSetupDataSO.Finish), new[] { typeof(MultiplayerResultsData) });
-        }
-        
         [HarmonyPrefix]
         [HarmonyPatch(typeof(StandardLevelScenesTransitionSetupDataSO), nameof(StandardLevelScenesTransitionSetupDataSO.Finish), MethodType.Normal)]
         static void Prefix(StandardLevelScenesTransitionSetupDataSO __instance, LevelCompletionResults levelCompletionResults)
